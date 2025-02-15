5eb24cc118576f824788e73ed87cb9413dc8c46b
Jaden ACHAIN <jac@originaldog.fr>
2025-02-15 13:59:44 +0100

    test changed/unchanged

--- detections/management/commands/dataset-detection.py
+++ detections/management/commands/dataset-detection.py
@@ -105,8 +105,7 @@ class Command(BaseCommand):
         family_type = 'all'
         family_indexes = [0]
 
-        type_video_percent = 0.1
-        changed_percent = 0.8
+        type_video_percent = 0.2
         type_vision_percent = 1 - type_video_percent
         dataset_min_count = 1000
 
@@ -127,13 +126,16 @@ class Command(BaseCommand):
 
         df['filename'] = df['photo_file'].apply(replace_extension)
         df['ext'] = df['photo_file'].apply(get_extension)
+        df['changed'] = df['changed'].astype(bool)
         df['image_path'] = './static/captures/' + df['status'] + '/images/' + df['photo_file']
         df['label_path'] = './static/captures/' + df['status'] + '/labels/' + df['photo_file'].apply(
             replace_extension,
             new_ext=".txt")
 
-        df_vision = df[df['source'] == 'vision'].sample(frac=1, random_state=42).reset_index(drop=True)
-        df_video = df[df['source'] == 'video'].sample(frac=1, random_state=42).reset_index(drop=True)
+        df_vision = df[(df['source'] == 'vision') & (df['changed'])].sample(frac=1, random_state=42).reset_index(
+            drop=True)
+        df_video = df[df['source'] == 'video'] \
+            .sample(frac=1, random_state=42).reset_index(drop=True)
 
         if (df_vision.shape[0] > 0) & (df_video.shape[0] > 0):
             if df['source'].value_counts(normalize=True).loc['video'] < type_video_percent:
@@ -147,47 +149,26 @@ class Command(BaseCommand):
                     random_state=42
                 )
 
-            df_vision_changed = df_vision[df_vision['changed']]
-            df_vision_unchanged = df_vision[~df_vision['changed']]
-
-            if df_vision['changed'].value_counts(normalize=True).loc[True] < changed_percent:
-                df_vision_unchanged = df_vision_unchanged.sample(
-                    n=int(df_vision_changed.shape[0] * (1 - changed_percent) / changed_percent),
-                    random_state=42
-                )
-            else:
-                df_vision_changed = df_vision_changed.sample(
-                    n=int(df_vision_unchanged.shape[0] * (1 - changed_percent) / changed_percent),
-                    random_state=42
-                )
-
-            if (df_video.shape[0] + df_vision_changed.shape[0] + df_vision_unchanged.shape[0]) > dataset_min_count:
+            if (df_video.shape[0] + df_vision.shape[0]) > dataset_min_count:
                 video_val_count = floor(df_video.shape[0] * dataset_val_percent)
-                vision_changed_val_count = floor(df_vision_changed.shape[0] * dataset_val_percent)
-                vision_unchanged_val_count = floor(df_vision_unchanged.shape[0] * dataset_val_percent)
+                vision_val_count = floor(df_vision.shape[0] * dataset_val_percent)
                 video_test_count = floor(df_video.shape[0] * dataset_test_percent)
-                vision_changed_test_count = floor(df_vision_changed.shape[0] * dataset_test_percent)
-                vision_unchanged_test_count = floor(df_vision_unchanged.shape[0] * dataset_test_percent)
+                vision_test_count = floor(df_vision.shape[0] * dataset_test_percent)
 
                 df_val = pd.concat([
-                    df_vision_changed.iloc[:vision_changed_val_count],
-                    df_vision_unchanged.iloc[:vision_unchanged_val_count],
+                    df_vision.iloc[:vision_val_count],
                     df_video.iloc[:video_val_count],
                 ]).reset_index(drop=True)
                 df_val['type'] = 'val'
 
                 df_test = pd.concat([
-                    df_vision_changed.iloc[
-                    vision_changed_val_count:vision_changed_val_count + vision_changed_test_count],
-                    df_vision_unchanged.iloc[
-                    vision_unchanged_val_count:vision_unchanged_val_count + vision_unchanged_test_count],
+                    df_vision.iloc[vision_val_count:vision_val_count + vision_test_count],
                     df_video.iloc[video_val_count:video_val_count + video_test_count],
                 ]).reset_index(drop=True)
                 df_test['type'] = 'test'
 
                 df_train = pd.concat([
-                    df_vision_changed.iloc[vision_changed_val_count + vision_changed_test_count:],
-                    df_vision_unchanged.iloc[vision_unchanged_val_count + vision_unchanged_test_count:],
+                    df_vision.iloc[vision_val_count + vision_test_count:],
                     df_video.iloc[video_val_count + video_test_count:],
                 ]).reset_index(drop=True)
                 df_train['type'] = 'train'
@@ -205,8 +186,7 @@ class Command(BaseCommand):
                     copy_to(df_test, f'{dataset_dir}/test')
                     copy_to(df_train, f'{dataset_dir}/train')
 
-                    capture_ids = df_vision_changed['id'].to_list() + df_vision_unchanged['id'].to_list() + df_video[
-                        'id'].to_list()
+                    capture_ids = df_vision['id'].to_list() + df_video['id'].to_list()
                     Capture.objects.filter(id__in=capture_ids) \
                         .update(**{family_type_db: True})
 
@@ -233,10 +213,12 @@ class Command(BaseCommand):
                             '[DEMO]' if not active else '') + f'[{chunk_number}] Successfully finished with {df_vision.shape[0] + df_video.shape[0]} items')
                 )
             else:
+                print(df['source'].value_counts(normalize=True))
+                print(df['changed'].value_counts(normalize=False))
                 self.stdout.write(
                     self.style.ERROR(
                         (
-                            '[DEMO]' if not active else '') + f'Not enough items: {df_vision_changed.shape[0] + df_vision_unchanged.shape[0] + df_video.shape[0]} ')
+                            '[DEMO]' if not active else '') + f'Not enough items: [vision] {df_vision.shape[0]} / [video] {df_video.shape[0]} ')
                 )
         else:
             self.stdout.write(
